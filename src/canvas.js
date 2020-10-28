bab465a4a098d239b3383bf8a79d91baf6a302cf
vp2350 <42982315+vp2350@users.noreply.github.com>
2020-10-28 03:00:40 -0400

    Add files via upload

--- src/canvas.js
+++ src/canvas.js
@@ -9,8 +9,7 @@
 
 import * as utils from './utils.js';
 
-let ctx,canvasWidth,canvasHeight,gradient,analyserNode,audioData;
-
+let ctx,canvasWidth,canvasHeight,gradient,analyserNode,audioData, image;
 
 function setupCanvas(canvasElement,analyserNodeRef){
 	// create drawing context
@@ -23,32 +22,45 @@ function setupCanvas(canvasElement,analyserNodeRef){
 	analyserNode = analyserNodeRef;
 	// this is the array where the analyser data will be stored
 	audioData = new Uint8Array(analyserNode.fftSize/2);
+
 }
 
 function draw(params={}){
   // 1 - populate the audioData array with the frequency data from the analyserNode
 	// notice these arrays are passed "by reference" 
-	analyserNode.getByteFrequencyData(audioData);
-	// OR
-	//analyserNode.getByteTimeDomainData(audioData); // waveform data
-	
+    ctx.clearRect(0,0,canvasWidth,canvasHeight);
+	if(params.useWaveForm) analyserNode.getByteTimeDomainData(audioData);
+	else analyserNode.getByteFrequencyData(audioData);// waveform data
 	// 2 - draw background
 	ctx.save();
     ctx.fillStyle = "black";
-    ctx.globalAlpha=.1;
+    ctx.globalAlpha=0.8;
     ctx.fillRect(0,0,canvasWidth, canvasHeight);
+    ctx.restore();
+    
+    ctx.save();
+    if(params.duration != null)
+    {
+        ctx.fillStyle = "red";
+        ctx.globalAlpha= 1;
+        ctx.fillRect(10, 10, 500, 20);
+        ctx.fillStyle = "green";
+        ctx.fillRect(10, 10, (params.currentTime/params.duration) * 500, 20)
+        ctx.fillText((Math.floor(params.currentTime/60))+":"+(params.currentTime%60) + "/"+ Math.floor(params.duration/60)+":"+(params.duration%60), 520, 20);
+    }
     ctx.restore();
 		
 	// 3 - draw gradient
 	if(params.showGradient){
         ctx.save();
         ctx.fillStyle = gradient;
-        ctx.globalAlpha = .3;
+        ctx.globalAlpha = .5;
         ctx.fillRect(0,0,canvasWidth,canvasHeight);
         ctx.restore();
     }
 	// 4 - draw bars
 	if(params.showBars){
+        let negative = -1;
         let barSpacing = 4;
         let margin = 5;
         let screenWidthForBars = canvasWidth - (audioData.length*barSpacing) - margin*2;
@@ -57,12 +69,68 @@ function draw(params={}){
         let topSpacing=100;
         
         ctx.save();
-        ctx.fillStyle='rgba(255,255,255,0.50)';
-        ctx.strokeStyle='rgba(0,0,0, 0.50)';
+        ctx.fillStyle='rgba(255,255,255,0.90)';
+        ctx.strokeStyle='rgba(0,255,0, 0.90)';
         //loop through the data and draw!
-        for(let i=0; i<audioData.length; i++){
-            ctx.fillRect(margin + i * (barWidth+barSpacing),topSpacing+256-audioData[i], barWidth, barHeight);
-            ctx.strokeRect(margin + i * (barWidth+barSpacing), topSpacing+256 - audioData[i], barWidth, barHeight);
+        for(let i=1; i<audioData.length; i++){
+            //ctx.fillRect(margin + i * (barWidth+barSpacing),topSpacing+256-audioData[i], barWidth, barHeight);
+            //ctx.strokeRect(margin + i * (barWidth+barSpacing), topSpacing+256 - audioData[i], barWidth, barHeight);
+            ctx.beginPath();
+            ctx.moveTo(margin + (i-1)* (barWidth+barSpacing), (canvasHeight/2 - (audioData[i-1]*negative*-1)*0.5));
+            ctx.lineTo(margin + (i)* (barWidth+barSpacing), (canvasHeight/2 -(audioData[i]*negative)*0.5));
+            ctx.stroke();
+
+            negative = negative*-1;
+            
+        }
+        ctx.restore();
+    }
+    
+    if(params.bouncingLines){
+        let negative = -1;
+        let barSpacing = 4;
+        let margin = 5;
+        let screenWidthForBars = canvasWidth - (audioData.length*barSpacing) - margin*2;
+        let barWidth = screenWidthForBars/audioData.length;
+        
+        ctx.save();
+        ctx.fillStyle='rgba(255,255,255,0.80)';
+        ctx.strokeStyle='rgba(255,0,0, 0.80)';
+        //loop through the data and draw!
+        for(let i=1; i<audioData.length; i++){
+            //ctx.fillRect(margin + i * (barWidth+barSpacing),topSpacing+256-audioData[i], barWidth, barHeight);
+            //ctx.strokeRect(margin + i * (barWidth+barSpacing), topSpacing+256 - audioData[i], barWidth, barHeight);
+            ctx.beginPath();
+            ctx.moveTo(margin + (i-1)* (barWidth+barSpacing), canvasHeight/2 - (audioData[i-1]*negative));
+            ctx.lineTo(margin + (i)* (barWidth+barSpacing), canvasHeight/2 -(audioData[i]*negative));
+            ctx.stroke();
+            negative = negative*-1;
+        
+        }
+        ctx.restore();
+    }
+    
+    if(params.quadraticCurves)
+    {
+        let negative = -1;
+        let barSpacing = 4;
+        let margin = 5;
+        let screenWidthForBars = canvasWidth - (audioData.length*barSpacing) - margin*2;
+        let barWidth = screenWidthForBars/audioData.length;
+        let barHeight=200;
+        
+        ctx.save();
+        ctx.fillStyle='rgba(255,255,255,0.80)';
+        for(let i=1; i<audioData.length; i++){
+            ctx.strokeStyle=`rgba(${255-i},${0+i}, ${0+ 2*i}, 0.80)`;
+            
+            ctx.beginPath();
+            ctx.moveTo(canvasWidth/2, 0);
+            ctx.quadraticCurveTo(canvasWidth/2 - ((audioData[i-1]*negative)*5), canvasHeight/2 , canvasWidth/2, canvasHeight);
+            ctx.stroke();
+            
+            negative = negative*-1;
+            
         }
         ctx.restore();
     }
@@ -76,24 +144,65 @@ function draw(params={}){
             let percent= audioData[i]/255;
             
             let circleRadius = percent*maxRadius;
-            ctx.beginPath();
-            ctx.fillStyle=utils.makeColor(255,111,111, .34-percent/3.0);
-            ctx.arc(canvasWidth/2, canvasHeight/2, circleRadius, 0, 2 * Math.PI, false);
-            ctx.fill();
-            ctx.closePath();
             
             //blue-ish circles, bigger, more transparent
             ctx.beginPath();
-            ctx.fillStyle=utils.makeColor(0,0,255,.10-percent/10.0);
-            ctx.arc(canvasWidth/2, canvasHeight/2, circleRadius*1.5,0,2*Math.PI, false);
+            ctx.fillStyle=utils.makeColor(0,0,255,.15-percent/10.0);
+            ctx.arc(canvasWidth, canvasHeight/2, circleRadius,0,2*Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+            
+            ctx.beginPath();
+            ctx.fillStyle=utils.makeColor(255,111,111, .05-percent/3.0);
+            ctx.arc(canvasWidth, canvasHeight/2, circleRadius * 1.5, 0, 2 * Math.PI, false);
             ctx.fill();
             ctx.closePath();
             
             //yellow-ish circles, smaller
             ctx.save();
             ctx.beginPath();
-            ctx.fillStyle=utils.makeColor(200,200,0,.5-percent/5.0);
+            ctx.fillStyle=utils.makeColor(200,200,0,.10-percent/5.0);
+            ctx.arc(canvasWidth, canvasHeight/2, circleRadius*.50, 0, 2*Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+            ctx.restore();
+            
+            ctx.beginPath();
+            ctx.fillStyle=utils.makeColor(255,111,111, .34-percent/3.0);
+            ctx.arc(canvasWidth/2, canvasHeight/2, circleRadius, 0, 2 * Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+            
+            ctx.beginPath();
+            ctx.fillStyle=utils.makeColor(0,0,255,.10-percent/10.0);
+            ctx.arc(canvasWidth/2, canvasHeight/2, circleRadius*1.5,0,2*Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+            
+            ctx.save();
+            ctx.beginPath();
+            ctx.fillStyle=utils.makeColor(200,200,0,.05-percent/5.0);
             ctx.arc(canvasWidth/2, canvasHeight/2, circleRadius*.50, 0, 2*Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+            ctx.restore();
+            
+            ctx.beginPath();
+            ctx.fillStyle=utils.makeColor(255,111,111, .10-percent/3.0);
+            ctx.arc(0, canvasHeight/2, circleRadius, 0, 2 * Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+            
+            ctx.beginPath();
+            ctx.fillStyle=utils.makeColor(0,0,255,.05-percent/10.0);
+            ctx.arc(0, canvasHeight/2, circleRadius*1.5,0,2*Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+            
+            ctx.save();
+            ctx.beginPath();
+            ctx.fillStyle=utils.makeColor(200,200,0,.34-percent/5.0);
+            ctx.arc(0, canvasHeight/2, circleRadius*.50, 0, 2*Math.PI, false);
             ctx.fill();
             ctx.closePath();
             ctx.restore();
@@ -118,12 +227,8 @@ function draw(params={}){
     for(let i=0; i< length; i+=4){
 		// C) randomly change every 20th pixel to red
 	    if(params.showNoise && Math.random() < 0.05){
-			// data[i] is the red channel
-			// data[i+1] is the green channel
-			// data[i+2] is the blue channel
-			// data[i+3] is the alpha channel
-			data[i] = data[i+1] = data[i+2] =0;// zero out the red and green and blue channels
-			data[i+1]=255;// make the green channel 100% green
+			data[i] = data[i+1] = data[i+2] =0;
+			data[i+1]=255;
 		} // end if
         
         if(params.showInvert){
@@ -132,6 +237,29 @@ function draw(params={}){
             data[i+1]=255-green;
             data[i+2]=255-blue;
         }
+		// magenta tint
+		if(params.showTint){
+            if(params.tintStyle=="Magenta")
+            {
+                data[i] += 50;  		// set red value
+                //data[i+1] += 50; 		// set green value
+                data[i+2] += 50;		// set blue value
+                //data[i+3] -= 128;		// set alpha value
+            }
+            else if(params.tintStyle == "Red")
+            {
+                data[i] += 50;  		// set red value
+            }
+            else if(params.tintStyle == "Green")
+            {
+                data[i+1] += 50;  		// set green value
+            }
+            else if(params.tintStyle == "Blue")
+            {
+                data[i+2] += 50;  		// set blue value
+            }
+		}
+		
 	} // end for
     if(params.showEmboss){
          for(let i=0; i<length; i++){
@@ -139,7 +267,6 @@ function draw(params={}){
              data[i] = 127 + 2*data[i] - data[i+4] - data[i+width*4];
          }
      }
-	
 	// D) copy image data back to canvas
     ctx.putImageData(imageData, 0, 0);
 }
--- src/main.js
+++ src/main.js
@@ -7,12 +7,19 @@
 // In this instance, we feel the code is more readable if written this way
 // If you want to re-write these as ES6 arrow functions, to be consistent with the other files, go ahead!
 const drawParams ={
-    showGradient: true,
-    showBars: true,
+    showGradient: false,
+    showBars: false,
     showCircles: true,
     showNoise: false,
     showInvert: false,
-    showEmboss: false
+    showEmboss: false,
+    showTint: false,
+    tintStyle: "Magenta",
+    duration: 0,
+    currentTime: 0,
+    bouncingLines: false,
+    useWaveForm: false,
+    quadraticCurves: true
 };
 
 import * as utils from './utils.js';
@@ -24,6 +31,13 @@ import * as canvas from './canvas.js';
 const DEFAULTS = Object.freeze({
 	sound1  :  "media/New Adventure Theme.mp3"
 });
+
+const playButton = document.querySelector("#playButton");    
+let trackSelect= document.querySelector("#trackSelect");
+let tintSelect= document.querySelector('input[name="tint"]:checked');
+let dataSelect= document.querySelector("#dataType");
+let freezeCooldown = false;
+let counter = 0;
 
 function init(){
     audio.setupWebAudio(DEFAULTS.sound1);
@@ -40,47 +54,141 @@ function init(){
           video.srcObject = stream;
         })
         .catch(function (err0r) {
-          console.log("Something went wrong!");
+          //console.log("Something went wrong!");
         });
     }
     loop();
+    update();
 }
 
+function update(){
+    if(audio.getDuration() != null)
+    {
+        drawParams.duration = Math.floor(audio.getDuration());
+        drawParams.currentTime = Math.floor(audio.getCurrent());
+    }
+    if(!freezeCooldown) counter-=2;
+    
+    if(counter>0)
+    {
+        document.querySelector("#cooldown").innerHTML = "Cooldown: "+  Math.floor(counter/60);
+    }
+    else
+    {
+        document.querySelector("#cooldown").innerHTML = "Cooldown: "+ 0;
+    }
+    requestAnimationFrame(update);
+    let label = document.querySelector("#label-container");
+    let handSign;
+    for(let i=0; i<label.childElementCount; i++)
+    {
+        let valueString = label.childNodes[i].innerHTML;
+        let floatValue=parseFloat(valueString.substring(valueString.length-4));
+        if(floatValue > 0.90)
+        {
+            let words = valueString.split(" ");
+            handSign = words[0];
+            console.log(handSign);
+        }
+    }
+    if(counter<0)
+    {
+        if(handSign == "Vulcan")
+        {
+            counter = 600;
+            trackSelect.value = "media/The Picard Song.mp3";
+            trackSelect.dispatchEvent(new MouseEvent("change"));
+            
+            console.log(`audioCtx.state before = ${audio.audioCtx.state}`);
+
+            //check if context is in suspended state (autoplay policy)
+            if(audio.audioCtx.state== "suspended"){
+                audio.audioCtx.resume();
+            }
+
+            console.log(`audioCtx.state after = ${audio.audioCtx.state}`);
+            //if(e.target.dataset.playing == "no"){
+                //if track is currently paused, play it
+                audio.playCurrentSound();
+                playButton.dataset.playing = "yes"; //our CSS will set the text to pause
+                //if track is playing, pause it
+            //
+            //else{
+            //    audio.pauseCurrentSound();
+            //    e.target.dataset.playing = "no"; //our CSS will set the text to play
+            //}    
+        }
+        else if(handSign == "Rock")
+        {
+            counter = 600;
+            trackSelect.value = "media/Peanuts Theme.mp3";
+            trackSelect.dispatchEvent(new MouseEvent("change"));
+
+            console.log(`audioCtx.state before = ${audio.audioCtx.state}`);
+
+            //check if context is in suspended state (autoplay policy)
+            if(audio.audioCtx.state== "suspended"){
+                audio.audioCtx.resume();
+            }
+
+            console.log(`audioCtx.state after = ${audio.audioCtx.state}`);
+            //if(e.target.dataset.playing == "no"){
+                //if track is currently paused, play it
+                audio.playCurrentSound();
+                playButton.dataset.playing = "yes"; //our CSS will set the text to pause
+                //if track is playing, pause it
+            //
+            //else{
+            //    audio.pauseCurrentSound();
+            //    e.target.dataset.playing = "no"; //our CSS will set the text to play
+            //}    
+        }
+        else if(handSign == "Thumbs")
+        {
+            counter = 600;
+            trackSelect.value = "media/New Adventure Theme.mp3";
+            trackSelect.dispatchEvent(new MouseEvent("change"));
+
+            console.log(`audioCtx.state before = ${audio.audioCtx.state}`);
+
+            //check if context is in suspended state (autoplay policy)
+            if(audio.audioCtx.state== "suspended"){
+                audio.audioCtx.resume();
+            }
+
+            console.log(`audioCtx.state after = ${audio.audioCtx.state}`);
+            //if(e.target.dataset.playing == "no"){
+                //if track is currently paused, play it
+                audio.playCurrentSound();
+                playButton.dataset.playing = "yes"; //our CSS will set the text to pause
+                //if track is playing, pause it
+            //
+            //else{
+            //    audio.pauseCurrentSound();
+            //    e.target.dataset.playing = "no"; //our CSS will set the text to play
+            //}    
+        }
+    }
+}
 function loop(){
-/* NOTE: This is temporary testing code that we will delete in Part II */
+    
 	requestAnimationFrame(loop);
+    tintSelect= document.querySelector('input[name="tint"]:checked');
+	drawParams.tintStyle =tintSelect.value;
     canvas.draw(drawParams);
 
 	// 1) create a byte array (values of 0-255) to hold the audio data
 	// normally, we do this once when the program starts up, NOT every frame
 	let audioData = new Uint8Array(audio.analyserNode.fftSize/2);
-	
+
 	// 2) populate the array of audio data *by reference* (i.e. by its address)
 	audio.analyserNode.getByteFrequencyData(audioData);
-	
-	// 3) log out the array and the average loudness (amplitude) of all of the frequency bins
-		//console.log(audioData);
-		//
-		//console.log("-----Audio Stats-----");
-		//let totalLoudness =  audioData.reduce((total,num) => total + num);
-		//let averageLoudness =  totalLoudness/(audio.analyserNode.fftSize/2);
-		//let minLoudness =  Math.min(...audioData); // ooh - the ES6 spread operator is handy!
-		//let maxLoudness =  Math.max(...audioData); // ditto!
-		//// Now look at loudness in a specific bin
-		//// 22050 kHz divided by 128 bins = 172.23 kHz per bin
-		//// the 12th element in array represents loudness at 2.067 kHz
-		//let loudnessAt2K = audioData[11]; 
-		//console.log(`averageLoudness = ${averageLoudness}`);
-		//console.log(`minLoudness = ${minLoudness}`);
-		//console.log(`maxLoudness = ${maxLoudness}`);
-		//console.log(`loudnessAt2K = ${loudnessAt2K}`);
-		//console.log("---------------------");
+
 }
 
 function setupUI(canvasElement){
     // A - hookup fullscreen button
     const fsButton = document.querySelector("#fsButton");
-    const playButton = document.querySelector("#playButton");    
     // add .onclick event to button
     fsButton.onclick = e => {
       console.log("init called");
@@ -93,6 +201,9 @@ function setupUI(canvasElement){
     document.querySelector('#barsCB').onchange = e => {
       drawParams.showBars = e.target.checked;
     };
+    document.querySelector('#bouncingCB').onchange = e => {
+      drawParams.bouncingLines = e.target.checked;
+    };
     document.querySelector('#circlesCB').onchange = e => {
       drawParams.showCircles = e.target.checked;
     };
@@ -104,6 +215,15 @@ function setupUI(canvasElement){
     };
     document.querySelector('#embossCB').onchange = e => {
       drawParams.showEmboss = e.target.checked;
+    };
+    document.querySelector('#tintCB').onchange = e => {
+      drawParams.showTint = e.target.checked;
+    };
+    document.querySelector('#curvesCB').onchange = e => {
+      drawParams.quadraticCurves = e.target.checked;
+    };
+    document.querySelector('#cooldownCB').onchange = e => {
+      freezeCooldown = e.target.checked;
     };
     //add .onclick event to button       
     playButton.onclick = e =>{
@@ -146,7 +266,6 @@ function setupUI(canvasElement){
    
 	  
     //D - hookup track select
-    let trackSelect= document.querySelector("#trackSelect");
     //add .onchange event to select
     trackSelect.onchange = e =>{
         audio.loadSoundFile(e.target.value);
@@ -155,6 +274,30 @@ function setupUI(canvasElement){
             playButton.dispatchEvent(new MouseEvent("click"));
         };
     }
+    
+    dataSelect.onchange = e =>{
+        if(dataSelect.value == "waveform")
+        {
+            drawParams.useWaveForm = true;
+        }
+        else
+        {
+            drawParams.useWaveForm = false;
+        }
+    }
+    //tintSelect.onchange = e =>{
+    //    console.log(e.target.value);
+    //    drawParams.tintStyle = e.target.value;
+    //}
+    console.log(tintSelect.value);
+    //for(let i=0;i<tintSelect.length;i++)
+    //    {
+    //        console.log(tintSelect[i]).value;
+    //        if(tintSelect[i].checked)
+    //            {
+    //                drawParams.tintStyle = tintSelect[i].value;
+    //            }
+    //    }
 } // end setupUI
 
 export {init};
